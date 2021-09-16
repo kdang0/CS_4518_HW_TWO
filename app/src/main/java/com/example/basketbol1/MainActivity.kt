d860d2f65042458613c4837f17fa7391054a9e92
kdang0 <kdang@wpi.edu>
2021-09-16 14:47:16 -0400

    PART 2

--- app/src/main/java/com/example/basketbol1/MainActivity.kt
+++ app/src/main/java/com/example/basketbol1/MainActivity.kt
@@ -18,161 +18,148 @@ import com.example.basketbol1.Team
 //import com.example.myapplication.R
 //import com.example.myapplication.Team
 
-private const val TAG = "MainActivity"
-private const val KEY_AScore = "AScore"
-private const val KEY_BScore = "BScore"
-private const val REQUEST_CODE_CLICKED = 0
+//private const val TAG = "MainActivity"
+//private const val KEY_AScore = "AScore"
+//private const val KEY_BScore = "BScore"
+//private const val REQUEST_CODE_CLICKED = 0
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textA: TextView
-    private lateinit var textAPTS: TextView
-    private lateinit var textB: TextView
-    private lateinit var textBPTS: TextView
-    private lateinit var buttonAPT3: Button
-    private lateinit var buttonAPT2: Button
-    private lateinit var buttonAPT1: Button
-    private lateinit var buttonBPT3: Button
-    private lateinit var buttonBPT2: Button
-    private lateinit var buttonBPT1: Button
-    private lateinit var buttonReset: Button
-    private lateinit var displayButton : Button
-    private val basketbolViewModel: BasketbolViewModel by lazy {
-        ViewModelProviders.of(this).get(BasketbolViewModel::class.java)
-    }
+//    private lateinit var textA: TextView
+//    private lateinit var textAPTS: TextView
+//    private lateinit var textB: TextView
+//    private lateinit var textBPTS: TextView
+//    private lateinit var buttonAPT3: Button
+//    private lateinit var buttonAPT2: Button
+//    private lateinit var buttonAPT1: Button
+//    private lateinit var buttonBPT3: Button
+//    private lateinit var buttonBPT2: Button
+//    private lateinit var buttonBPT1: Button
+//    private lateinit var buttonReset: Button
+//    private lateinit var displayButton : Button
+//    private val basketbolViewModel: BasketbolViewModel by lazy {
+//        ViewModelProviders.of(this).get(BasketbolViewModel::class.java)
+//   }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
+//        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "Got a BasketbolViewModel: $basketbolViewModel")
-        buttonAPT3 = findViewById(R.id.buttonAPT3)
-        buttonAPT2 = findViewById(R.id.buttonAPT2)
-        buttonAPT1 = findViewById(R.id.buttonAPT1)
-        buttonBPT3 = findViewById(R.id.buttonBPT3)
-        buttonBPT2 = findViewById(R.id.buttonBPT2)
-        buttonBPT1 = findViewById(R.id.buttonBPT1)
-        buttonReset = findViewById(R.id.buttonReset)
-        displayButton = findViewById(R.id.displayButton)
-        textA = findViewById(R.id.textA)
-        textAPTS = findViewById(R.id.textAPTS)
-        textB = findViewById(R.id.textB)
-        textBPTS = findViewById(R.id.textBPTS)
-        val aScore = savedInstanceState?.getInt(KEY_AScore, 0) ?: 0
-        basketbolViewModel.teams[0].score = aScore
-        textAPTS.text = basketbolViewModel.teamAPoints.toString()
-        val bScore = savedInstanceState?.getInt(KEY_BScore, 0) ?: 0
-        basketbolViewModel.teams[1].score = bScore
-        textBPTS.text = basketbolViewModel.teamBPoints.toString()
-        buttonAPT3.setOnClickListener {
-            basketbolViewModel.updatePts("A", 3)
-            textAPTS.text = basketbolViewModel.teamAPoints.toString()
-        }
-        buttonAPT2.setOnClickListener {
-            basketbolViewModel.updatePts("A", 2)
-            textAPTS.text = basketbolViewModel.teamAPoints.toString()
-        }
-        buttonAPT1.setOnClickListener {
-            basketbolViewModel.updatePts("A", 1)
-            textAPTS.text = basketbolViewModel.teamAPoints.toString()
-        }
-        buttonBPT3.setOnClickListener {
-            basketbolViewModel.updatePts("B", 3)
-            textBPTS.text = basketbolViewModel.teamBPoints.toString()
-        }
-        buttonBPT2.setOnClickListener {
-            basketbolViewModel.updatePts("B", 2)
-            textBPTS.text = basketbolViewModel.teamBPoints.toString()
-        }
-        buttonBPT1.setOnClickListener {
-            basketbolViewModel.updatePts("B", 1)
-            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(currentFragment == null){
+            val fragment = main_fragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
         }
 
-        buttonReset.setOnClickListener {
-            basketbolViewModel.reset()
-            textAPTS.text = basketbolViewModel.teamAPoints.toString()
-            textBPTS.text = basketbolViewModel.teamBPoints.toString()
-        }
-
-        displayButton.setOnClickListener {
-            val isClicked = basketbolViewModel.butIsClicked
-            val intent = BASKETBOL2.newIntent(this@MainActivity,basketbolViewModel.butIsClicked)
-            startActivityForResult(intent, REQUEST_CODE_CLICKED)
-
-        }
+//        Log.d(TAG, "Got a BasketbolViewModel: $basketbolViewModel")
+//        buttonAPT3 = findViewById(R.id.buttonAPT3)
+//        buttonAPT2 = findViewById(R.id.buttonAPT2)
+//        buttonAPT1 = findViewById(R.id.buttonAPT1)
+//        buttonBPT3 = findViewById(R.id.buttonBPT3)
+//        buttonBPT2 = findViewById(R.id.buttonBPT2)
+//        buttonBPT1 = findViewById(R.id.buttonBPT1)
+//        buttonReset = findViewById(R.id.buttonReset)
+//        displayButton = findViewById(R.id.displayButton)
+//        textA = findViewById(R.id.textA)
+//        textAPTS = findViewById(R.id.textAPTS)
+//        textB = findViewById(R.id.textB)
+//        textBPTS = findViewById(R.id.textBPTS)
+//        val aScore = savedInstanceState?.getInt(KEY_AScore, 0) ?: 0
+//        basketbolViewModel.teams[0].score = aScore
+//        textAPTS.text = basketbolViewModel.teamAPoints.toString()
+//        val bScore = savedInstanceState?.getInt(KEY_BScore, 0) ?: 0
+//        basketbolViewModel.teams[1].score = bScore
+//        textBPTS.text = basketbolViewModel.teamBPoints.toString()
+//        buttonAPT3.setOnClickListener {
+//            basketbolViewModel.updatePts("A", 3)
+//            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+//        }
+//        buttonAPT2.setOnClickListener {
+//            basketbolViewModel.updatePts("A", 2)
+//            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+//        }
+//        buttonAPT1.setOnClickListener {
+//            basketbolViewModel.updatePts("A", 1)
+//            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+//        }
+//        buttonBPT3.setOnClickListener {
+//            basketbolViewModel.updatePts("B", 3)
+//            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+//        }
+//        buttonBPT2.setOnClickListener {
+//            basketbolViewModel.updatePts("B", 2)
+//            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+//        }
+//        buttonBPT1.setOnClickListener {
+//            basketbolViewModel.updatePts("B", 1)
+//            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+//        }
+//
+//        buttonReset.setOnClickListener {
+//            basketbolViewModel.reset()
+//            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+//            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+//        }
+//
+//        displayButton.setOnClickListener {
+//            val isClicked = basketbolViewModel.butIsClicked
+//            val intent = BASKETBOL2.newIntent(this@MainActivity,basketbolViewModel.butIsClicked)
+//            startActivityForResult(intent, REQUEST_CODE_CLICKED)
+//
+//        }
 
 
 
     }
 
-    override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
-      super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode != Activity.RESULT_OK)
-            {return}
-        if(requestCode == REQUEST_CODE_CLICKED){
-            basketbolViewModel.butIsClicked = data?.getBooleanExtra(IS_CLICKED_TWO, false)?:false
-            val resultClick = when {
-                basketbolViewModel.butIsClicked -> R.string.clickB
-                else -> R.string.clickA
-            }
-            displayButton.setText(resultClick)
-            Log.d(TAG, "updated button on homepage");
-        }
-    }
-
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-
-/*
-    private fun updatePts(Team: String, Points: Int) {
-        if (Team == "A") {
-            teams[0].score = Points + teams[0].score
-            textAPTS.setText(teams[0].score.toString())
-        } else {
-            teams[1].score = Points + teams[1].score
-            textBPTS.setText(teams[1].score.toString())
-        }
-    }
-
-    private fun reset() {
-        teams[0].score = 0
-        teams[1].score = 0
-        textAPTS.setText(teams[0].score.toString())
-        textBPTS.setText(teams[1].score.toString())
-}
-
- */
-        override fun onSaveInstanceState(savedInstanceState: Bundle) {
-            super.onSaveInstanceState(savedInstanceState)
-            Log.i(TAG, "onSavedInstanceState")
-            savedInstanceState.putInt(KEY_AScore, basketbolViewModel.teams[0].score)
-            savedInstanceState.putInt(KEY_BScore, basketbolViewModel.teams[1].score)
-
-        }
+//    override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
+//      super.onActivityResult(requestCode, resultCode, data)
+//        if(resultCode != Activity.RESULT_OK)
+//            {return}
+//        if(requestCode == REQUEST_CODE_CLICKED){
+//            basketbolViewModel.butIsClicked = data?.getBooleanExtra(IS_CLICKED_TWO, false)?:false
+//            val resultClick = when {
+//                basketbolViewModel.butIsClicked -> R.string.clickB
+//                else -> R.string.clickA
+//            }
+//            displayButton.setText(resultClick)
+//            Log.d(TAG, "updated button on homepage");
+//        }
+//    }
+//
+//
+//    override fun onStart() {
+//        super.onStart()
+//        Log.d(TAG, "onStart() called")
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        Log.d(TAG, "onPause() called")
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        Log.d(TAG, "onResume() called")
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Log.d(TAG, "onDestroy() called")
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        Log.d(TAG, "onStop() called")
+//    }
+//
+//        override fun onSaveInstanceState(savedInstanceState: Bundle) {
+//            super.onSaveInstanceState(savedInstanceState)
+//            Log.i(TAG, "onSavedInstanceState")
+//            savedInstanceState.putInt(KEY_AScore, basketbolViewModel.teams[0].score)
+//            savedInstanceState.putInt(KEY_BScore, basketbolViewModel.teams[1].score)
+//
+//        }
 
     }
 
---
+++ app/src/main/java/com/example/basketbol1/main_fragment.kt
@@ -0,0 +1,150 @@
+package com.example.basketbol1
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+private const val TAG = "MainActivity"
+private const val KEY_AScore = "AScore"
+private const val KEY_BScore = "BScore"
+private const val REQUEST_CODE_CLICKED = 0
+class main_fragment : Fragment() {
+    private lateinit var textA: TextView
+    private lateinit var textAPTS: TextView
+    private lateinit var textB: TextView
+    private lateinit var textBPTS: TextView
+    private lateinit var buttonAPT3: Button
+    private lateinit var buttonAPT2: Button
+    private lateinit var buttonAPT1: Button
+    private lateinit var buttonBPT3: Button
+    private lateinit var buttonBPT2: Button
+    private lateinit var buttonBPT1: Button
+    private lateinit var buttonReset: Button
+    private lateinit var displayButton : Button
+    private val basketbolViewModel: BasketbolViewModel by lazy {
+        ViewModelProviders.of(this).get(BasketbolViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        Log.d(TAG, "Got a BasketbolViewModel: $basketbolViewModel")
+        val aScore = savedInstanceState?.getInt(KEY_AScore, 0) ?: 0
+        basketbolViewModel.teams[0].score = aScore
+        textAPTS.text = basketbolViewModel.teamAPoints.toString()
+        val bScore = savedInstanceState?.getInt(KEY_BScore, 0) ?: 0
+        basketbolViewModel.teams[1].score = bScore
+        textBPTS.text = basketbolViewModel.teamBPoints.toString()
+        buttonAPT3.setOnClickListener {
+            basketbolViewModel.updatePts("A", 3)
+            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+        }
+        buttonAPT2.setOnClickListener {
+            basketbolViewModel.updatePts("A", 2)
+            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+        }
+        buttonAPT1.setOnClickListener {
+            basketbolViewModel.updatePts("A", 1)
+            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+        }
+        buttonBPT3.setOnClickListener {
+            basketbolViewModel.updatePts("B", 3)
+            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+        }
+        buttonBPT2.setOnClickListener {
+            basketbolViewModel.updatePts("B", 2)
+            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+        }
+        buttonBPT1.setOnClickListener {
+            basketbolViewModel.updatePts("B", 1)
+            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+        }
+
+        buttonReset.setOnClickListener {
+            basketbolViewModel.reset()
+            textAPTS.text = basketbolViewModel.teamAPoints.toString()
+            textBPTS.text = basketbolViewModel.teamBPoints.toString()
+        }
+
+        /*
+        displayButton.setOnClickListener {
+            val isClicked = basketbolViewModel.butIsClicked
+            val intent = BASKETBOL2.newIntent(this@main_fragment,basketbolViewModel.butIsClicked)
+            startActivityForResult(intent, REQUEST_CODE_CLICKED)
+        }
+         */
+    }
+
+    override fun onCreateView(inflater : LayoutInflater, container : ViewGroup?, savedInstanceState: Bundle?) : View?{
+        val view = inflater.inflate(R.layout.fragment_basketbol, container,false)
+        buttonAPT3 = view.findViewById(R.id.buttonAPT3) as Button
+        buttonAPT2 = view.findViewById(R.id.buttonAPT2) as Button
+        buttonAPT1 = view.findViewById(R.id.buttonAPT1) as Button
+        buttonBPT3 = view.findViewById(R.id.buttonBPT3) as Button
+        buttonBPT2 = view.findViewById(R.id.buttonBPT2) as Button
+        buttonBPT1 = view.findViewById(R.id.buttonBPT1) as Button
+        buttonReset = view.findViewById(R.id.buttonReset) as Button
+        displayButton = view.findViewById(R.id.displayButton) as Button
+        textA = view.findViewById(R.id.textA) as TextView
+        textAPTS = view.findViewById(R.id.textAPTS) as TextView
+        textB = view.findViewById(R.id.textB) as TextView
+        textBPTS = view.findViewById(R.id.textBPTS) as TextView
+        return view
+    }
+
+    override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode != Activity.RESULT_OK)
+        {return}
+        if(requestCode == REQUEST_CODE_CLICKED){
+            basketbolViewModel.butIsClicked = data?.getBooleanExtra(IS_CLICKED_TWO, false)?:false
+            val resultClick = when {
+                basketbolViewModel.butIsClicked -> R.string.clickB
+                else -> R.string.clickA
+            }
+            displayButton.setText(resultClick)
+            Log.d(TAG, "updated button on homepage");
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSavedInstanceState")
+        savedInstanceState.putInt(KEY_AScore, basketbolViewModel.teams[0].score)
+        savedInstanceState.putInt(KEY_BScore, basketbolViewModel.teams[1].score)
+
+    }
+}
