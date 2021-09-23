e14baeec36f0228f4133d86a015a2b9cf0f62ae0
kdang0 <kdang@wpi.edu>
2021-09-23 16:04:36 -0400

    feature/Part5

--- app/src/main/java/com/example/basketbol1/BBGame.kt
+++ app/src/main/java/com/example/basketbol1/BBGame.kt
@@ -10,7 +10,7 @@ data class BBGame(
     @PrimaryKey val id:UUID = UUID.randomUUID(),
     var teamAName : String = "A Team",
     var teamBName : String = "B team",
-    var teamAScore: Int = 1,
+    var teamAScore: Int = 0,
     var teamBScore: Int = 0,
     @ColumnInfo(name= "date") var date: Date = Date()
 )
